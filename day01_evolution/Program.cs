2c47171431c73e11f38bf1d3d3b49e7cc0aee617
Guidein7 <saikumartanakala2001@gmail.com>
2026-08-04 18:04:01 +0530

    Initial commit: day01 and day2prg projects

---
+++ day01_evolution/Program.cs
@@ -0,0 +1,143 @@
+﻿//practice activity
+
+// using System;
+
+// class Program
+// {
+//     static void Main()
+//     {
+//         Console.WriteLine("Welcome to .Net");
+//         Console.WriteLine("Learning .Net Full Stack Developer ");
+//     }
+// }
+
+// Coding Exercise
+
+// using System;
+
+// class Program
+// {
+//     static void Main()
+//     {
+//         Console.WriteLine("========================================");
+//         Console.WriteLine(" Name : sai kumar");
+//         Console.WriteLine("Course : .net Full Stack");
+//         Console.WriteLine("Platform : .Net ");
+//         Console.WriteLine("Language : C#");
+//         Console.WriteLine("Goal : Become Senior .Net Full Stack");
+//         Console.WriteLine("========================================");
+//     }
+// }
+
+
+
+// Company Challenge
+
+
+
+
+
+// using System;
+
+// class Program
+// {
+//     static void Main()
+//     {
+//         Console.WriteLine("========================================");
+//         Console.WriteLine("Company Name : Infosys");
+//         Console.WriteLine("Project Name : Employee Management System");
+//         Console.WriteLine("Technology : .Net Full Stack ");
+//         Console.WriteLine("Backend : Asap .Net Core");
+//         Console.WriteLine("Frontend : React");
+//         Console.WriteLine("Database : Sql Server");
+//         Console.WriteLine("Developer : sai kumar");
+//         Console.WriteLine("========================================");
+//     }
+// }
+
+
+
+// debuggging 
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Welcome to .NET");
+
+        Console.WriteLine("Learning C#");
+
+        Console.WriteLine("Bootcamp Started");
+    }
+}
+
+
+
+
+// using System;
+
+// class Program
+// {
+//     static void Main()
+//     {
+//         // Console.WriteLine("Evolution of .net");
+//         // Console.WriteLine("2002............net framework");
+//         // Console.WriteLine("2016........net core");
+//         // Console.WriteLine("2020......net 5");
+//         // Console.WriteLine("2022.......net 6");
+//         // Console.WriteLine("2023.....net 7");
+//         // Console.WriteLine("2024.....net 8");
+//         // Console.WriteLine("2025.....net 9");
+//         // Console.WriteLine("2026.....net 10");
+//         // Console.WriteLine("current learning  : modern .net");
+
+
+//         Console.WriteLine("first i am learning with .net framework");
+//         Console.WriteLine("second i am learning .net core");
+//         Console.WriteLine("third i am learning modern .net");
+//         Console.WriteLine("my final goal is : become a senior full stack dontnet developer");
+//     }
+// }
+
+
+
+// Debugging Challenge
+
+// using System;
+
+// class Program
+// {
+//     static void Main()
+//     {
+//         Console.WriteLine("NET Framework");
+
+//         Console.WriteLine(".NET Core");
+
+//         Console.WriteLine(".NET 8");
+//     }
+// }
+
+
+
+// Company Challenge?
+
+
+// using System;
+
+// class Program
+// {
+//     static void Main()
+//     {
+//         Console.WriteLine("===================================");
+//         Console.WriteLine("company name is IBM");
+//         Console.WriteLine("Migration Plan");
+//         Console.WriteLine("old platform  : .net framework 4.8");
+//         Console.WriteLine("current platform : .net 8");
+//         Console.WriteLine("Reason :  ✔ Better Performance \n ✔ Cross Platform  \n ✔ Cloud Ready");
+//         Console.WriteLine("========================");
+
+        
+//     }
+// }
---
+++ day2prg/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+class program
+{
+    static void Main()
+    {
+        Console.WriteLine("hello how are you");
+        string name = Console.ReadLine();
+        Console.WriteLine("welcome "+ name);
+        string value = Console.ReadLine();
+        Console.WriteLine("how old are you " + value);
+    }
+}
+
